51e554b84487d900a3f8a6ecdd90214f4945287c
AkadigitalProject <phucuong200297@gmail.com>
2023-03-12 01:53:41 +0700

    2023.local.4

--- plugins/cuongtest.js
+++ plugins/cuongtest.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+
 exports.register = function() {
-    this.logdebug('Hello world!');
+    this.logfile = fs.createWriteStream('Haraka/swaks.log', { flags: 'a' });
+};
+
+exports.hook_data_post = function(next, connection) {
+    const logline = `Date: ${new Date().toString()}, From: ${connection.transaction.mail_from}, To: ${connection.transaction.rcpt_to}`;
+    this.logfile.write(logline + '\n');
+    next();
 };
