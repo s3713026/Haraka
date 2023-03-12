caef1a6444fe48b7aa500edd06747c6683251528
AkadigitalProject <phucuong200297@gmail.com>
2023-03-12 13:47:39 +0700

    2023.local.14

--- plugins/cuongtest.js
+++ plugins/cuongtest.js
@@ -1,32 +1,10 @@
 const fs = require('fs');
-const nodemailer = require('nodemailer');
-
-// create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-    host: 'demo.akadigital.net',
-    port: 25,
-    secure: true, // true for 465, false for other ports
-});
-let mailOptions = {
-    from: 'cuong.test@demo.akadigital.net', // sender address
-    to: 'cuong.truong@akadigital.vn', // list of receivers
-    subject: 'Hello from Haraka!', // Subject line
-    text: 'Hello world!', // plain text body
-    html: '<b>Hello world!</b>' // html body
-};
 
 //Xác định plugin: 
 //Xác định plugin bằng cách sử dụng hàm export.register. 
 //Hàm này có hai tham số: tên plugin và chức năng xác định hành vi của plugin.
 exports.register = function() {
     this.logfile = fs.createWriteStream('swaks.log', { flags: 'a' });
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Message sent: %s', info.messageId);
-        }
-    });
 };
 
 //Khi Haraka nhận được email, hàm hook_data_post được gọi và chi tiết email được ghi vào tệp.
