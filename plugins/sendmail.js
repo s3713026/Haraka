e1511a15b59ad0fca63fd3b3a65eedfa5e4fbad7
AkadigitalProject <phucuong200297@gmail.com>
2023-03-13 13:14:16 +0700

    2023.local.20

--- plugins/sendmail.js
+++ plugins/sendmail.js
@@ -5,25 +5,25 @@ exports.register = function() {
 }
 
 exports.send_email = function(next, connection) {
-    var mail_from = connection.transaction.mail_from;
-    var rcpt_to = connection.transaction.rcpt_to;
-    var body = connection.transaction.message_stream.get_data();
+    // var mail_from = connection.transaction.mail_from;
+    // var rcpt_to = connection.transaction.rcpt_to;
+    // var body = connection.transaction.message_stream.get_data();
 
-    var transporter = nodemailer.createTransport({
-        host: 'demo.akadigital.net',
-        port: 25,
-        secure: true,
-        auth: {
-            user: 'cuong@demo.akadigital.com',
-            pass: 'password2'
-        }
-    });
+    // var transporter = nodemailer.createTransport({
+    //     host: 'demo.akadigital.net',
+    //     port: 25,
+    //     secure: true,
+    //     auth: {
+    //         user: 'cuong@demo.akadigital.com',
+    //         pass: 'password2'
+    //     }
+    // });
 
     var mailOptions = {
-        from: mail_from,
-        to: rcpt_to,
+        from: 'mail_from@demo.akadigital',
+        to: 'phucuong200297@gmail.com',
         subject: 'Test email from Haraka',
-        text: body
+        text: 'body'
     };
 
     transporter.sendMail(mailOptions, function(error, info) {
