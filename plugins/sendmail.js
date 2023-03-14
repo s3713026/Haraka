d74e4f735829704ba20888e4fa52e9352f2b2242
AkadigitalProject <phucuong200297@gmail.com>
2023-03-14 17:13:57 +0700

    2023.local.25

--- plugins/sendmail.js
+++ plugins/sendmail.js
@@ -3,13 +3,6 @@ const nodemailer = require('nodemailer');
 exports.register = function() {
     this.loginfo("HELLO CHAYJ ROOI")
     this.register_hook('queue', 'send_email');
-}
-
-exports.send_email = function(next, connection) {
-    // var mail_from = connection.transaction.mail_from;
-    // var rcpt_to = connection.transaction.rcpt_to;
-    // var body = connection.transaction.message_stream.get_data();
-
     var transporter = nodemailer.createTransport({
         host: '158.101.137.14',
         port: 25,
@@ -34,6 +27,38 @@ exports.send_email = function(next, connection) {
             console.log('Email sent: ' + info.response);
         }
     });
-
-    next();
+    next()
 }
+
+// exports.send_email = function(next, connection) {
+// var mail_from = connection.transaction.mail_from;
+// var rcpt_to = connection.transaction.rcpt_to;
+// var body = connection.transaction.message_stream.get_data();
+
+// var transporter = nodemailer.createTransport({
+//     host: '158.101.137.14',
+//     port: 25,
+// secure: true,
+// auth: {
+//     user: 'cuong@demo.akadigital.com',
+//     pass: 'password2'
+// }
+// });
+
+// var mailOptions = {
+//     from: 'mail_from@demo.akadigital',
+//     to: 'cuong.truong@akadigital.vn',
+//     subject: 'Test email from Haraka',
+//     text: 'body'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info) {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email sent: ' + info.response);
+//     }
+// });
+
+// next();
+// }
