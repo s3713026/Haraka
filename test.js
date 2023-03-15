980915cf00d76051d28d8d07622108e692c41581
AkadigitalProject <phucuong200297@gmail.com>
2023-03-15 17:29:17 +0700

    2023.local.25

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ let transporter = nodemailer.createTransport({
     port: 25,
     secure: false, // true for 465, false for other ports
     auth: {
-        user: 'cuong@demo.akadigital.com', // your Gmail email address
-        pass: 'password2' // your Gmail password
+        user: 'username1', // your Gmail email address
+        pass: 'akatestpassword' // your Gmail password
     }
 });
 
