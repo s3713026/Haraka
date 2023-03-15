8242f114ead6ee6bc7bd5b51d53973199c313b7f
AkadigitalProject <phucuong200297@gmail.com>
2023-03-15 17:34:47 +0700

    2023.local.25

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
     host: 'demo.akadigital.net',
     port: 25,
-    secure: false, // true for 465, false for other ports
+    secure: true, // true for 465, false for other ports
     auth: {
         user: 'username1', // your Gmail email address
         pass: 'akatestpassword' // your Gmail password
