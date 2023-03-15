607ec30671ecb7e594b9441c6527dd19334fa666
AkadigitalProject <phucuong200297@gmail.com>
2023-03-15 18:23:07 +0700

    2023.local.25

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-    host: 'localhost',
+    host: '158.101.137.14',
     port: 25,
     secure: false, // true for 465, false for other ports
     tls: {
