6feead3b8450b52a2460258f83140aece2328c07
AkadigitalProject <phucuong200297@gmail.com>
2023-03-15 18:04:16 +0700

    2023.local.25

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
     host: 'demo.akadigital.net',
-    port: 25,
+    port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
         user: 'username1', // your Gmail email address
