76370959cb101e854f8e3835fb4d34fe65440bfe
AkadigitalProject <phucuong200297@gmail.com>
2023-03-16 17:52:56 +0700

    2023.local.23

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const tls = require('tls');
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-    host: 'mail-smtp-in.l.google.com',
+    host: 'localhost',
     port: 25,
     secure: true, // true for 465, false for other ports
     auth: {
