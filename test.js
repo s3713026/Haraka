ebb513295a234f1d852ea8bc7d015209b04eb53e
AkadigitalProject <phucuong200297@gmail.com>
2023-03-16 15:32:25 +0700

    2023.local.25

--- test.js
+++ test.js
@@ -4,7 +4,10 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
     host: 'localhost',
     port: 25,
-
+    secure: true, // true for 465, false for other ports
+    tls: {
+        rejectUnauthorized: false
+    }
 });
 
 // setup email data with unicode symbols
