55e63f60c4273091a40e70d9365c49054aaed369
AkadigitalProject <phucuong200297@gmail.com>
2023-03-16 15:42:35 +0700

    2023.local.25

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ let transporter = nodemailer.createTransport({
     port: 25,
     secure: false, // true for 465, false for other ports
     tls: {
-        rejectUnauthorized: true
+        rejectUnauthorized: false
             // secureProtocol: 'TLSv1_2_method' // specify a compatible SSL/TLS version
     }
 });
