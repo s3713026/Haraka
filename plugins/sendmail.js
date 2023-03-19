635bccffabd04dc11d314397d0f32e18a544b08e
AkadigitalProject <phucuong200297@gmail.com>
2023-03-19 22:40:32 +0700

    2023.local.23

--- plugins/sendmail.js
+++ plugins/sendmail.js
@@ -1,34 +1,53 @@
-var outbound = require('./outbound');
+// Load required modules
+const SMTPClient = require('smtp-client').SMTPClient;
 
-var plugin = this;
+// Define plugin function
+exports.register = function() {
+    // Initialize plugin
+    const plugin = this;
 
-var to = 'phucuong200297@gmail.com';
-var from = 'cuong@demo.akadigital.net';
+    // Define delivery function
+    function deliver_recipient(next, connection, params) {
+        const rcpt = params[0];
+        const message = connection.transaction.message;
 
-var contents = [
-    "From: " + from,
-    "To: " + to,
-    "MIME-Version: 1.0",
-    "Content-type: text/plain; charset=us-ascii",
-    "Subject: Some subject here",
-    "",
-    "Some email body here",
-    ""
-].join("\n");
+        // Create SMTP client
+        const client = new SMTPClient({
+            host: rcpt.host,
+            port: rcpt.port || 25,
+            ssl: rcpt.secure || false,
+            auth: {
+                user: rcpt.user,
+                pass: rcpt.pass,
+            },
+        });
 
-var outnext = function(code, msg) {
-    switch (code) {
-        case DENY:
-            plugin.logerror("Sending mail failed: " + msg);
-            break;
-        case OK:
-            plugin.loginfo("mail sent");
-            next();
-            break;
-        default:
-            plugin.logerror("Unrecognized return code from sending email: " + msg);
-            next();
+        // Set up event handlers
+        client.on('error', (error) => {
+            connection.logerror(`Error delivering to ${rcpt.address}: ${error}`);
+            next(DENYSOFT, `Error delivering to ${rcpt.address}: ${error}`);
+        });
+
+        client.on('ready', () => {
+            client.useEnvelope({
+                from: message.header.headers.from,
+                to: rcpt.address,
+            });
+            client.startData(() => {
+                client.write(message.body);
+                client.end(() => {
+                    connection.loginfo(`Delivered to ${rcpt.address}`);
+                    next(OK);
+                });
+            });
+        });
+
+        // Connect to server
+        client.connect(() => {
+            connection.logdebug(`Connected to ${rcpt.host}:${rcpt.port}`);
+        });
     }
-};
 
-outbound.send_email(from, to, contents, outnext);
+    // Register plugins
+    plugin.register_hook('rcpt', 'outbound', deliver_recipient);
+};
