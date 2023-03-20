c1773611b7098d3eec341ef3fb51ff504270e885
AkadigitalProject <phucuong200297@gmail.com>
2023-03-20 15:32:59 +0700

    2023.local.27

--- test.js
+++ test.js
@@ -3,24 +3,37 @@ const app = express();
 // const haraka = require('haraka');
 
 // Define the API endpoint
-app.post('/api/send-email', (req, res) => {
-    // Validate the input data
-    const { from, to, subject, body } = req.body;
-    if (!from || !to || !subject || !body) {
-        res.status(400).send('Missing required fields');
-        return;
-    }
+// app.post('/api/send-email', (req, res) => {
+//     // Validate the input data
+//     const { from, to, subject, body } = req.body;
+//     if (!from || !to || !subject || !body) {
+//         res.status(400).send('Missing required fields');
+//         return;
+//     }
 
-    // Call the Haraka plugin function
-    // const plugin = haraka.plugin('sendmail');
-    // plugin.sendEmail(from, to, subject, body);
-    console.log("RUN API")
-        // Send a success response
-    res.status(200).send('Email sent successfully');
+//     // Call the Haraka plugin function
+//     // const plugin = haraka.plugin('sendmail');
+//     // plugin.sendEmail(from, to, subject, body);
+//     console.log("RUN API")
+//         // Send a success response
+//     res.status(200).send('Email sent successfully');
+// });
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+let items = [];
+
+app.get('/items', (req, res) => {
+    res.json(items);
 });
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+app.post('/items', (req, res) => {
+    const newItem = req.body;
+    items.push(newItem);
+    res.status(201).json(newItem);
+});
+
+app.listen(5000, () => {
+    console.log('Server started on port 3000');
 });
