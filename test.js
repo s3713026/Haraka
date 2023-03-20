7cea44497dcbaf65fc045376a536c027605e0e20
AkadigitalProject <phucuong200297@gmail.com>
2023-03-20 18:05:03 +0700

    2023.local.27

--- test.js
+++ test.js
@@ -1,37 +1,39 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
+
+
+// app.get('/', (req, res) => {
+//     res.send('SEND MAIL ĐI');
+//     console.log("Hello ")
+// });
+
+
+
+// app.listen(5000, () => {
+//     console.log('Server started on port 5000');
+// });
 const outbound = require('./outbound');
 
-app.get('/', (req, res) => {
-    res.send('SEND MAIL ĐI');
-    console.log("Hello ")
-    const from = 'sender@demo.akadigital.net';
-    const to = 'phucuong200297@gmail.com';
-    const subject = 'Test Email';
-    const body = 'This is a test email message.';
+const from = 'sender@demo.akadigital.net';
+const to = 'phucuong200297@gmail.com';
+const subject = 'Test Email';
+const body = 'This is a test email message.';
 
-    // var from = 'sender@demo.akadigital.net';
-    // var to = 'phucuong200297@gmail.com';
-    // var subject = 'Test Email';
-    // var body = 'This is a test email message.';
+// var from = 'sender@demo.akadigital.net';
+// var to = 'phucuong200297@gmail.com';
+// var subject = 'Test Email';
+// var body = 'This is a test email message.';
 
 
-    var contents = [
-        "From: " + from,
-        "To: " + to,
-        "MIME-Version: 1.0",
-        "Content-type: text/plain; charset=us-ascii",
-        "Subject:" + subject,
-        "",
-        body,
-        ""
-    ].join("\n");
+var contents = [
+    "From: " + from,
+    "To: " + to,
+    "MIME-Version: 1.0",
+    "Content-type: text/plain; charset=us-ascii",
+    "Subject:" + subject,
+    "",
+    body,
+    ""
+].join("\n");
 
-    outbound.send_email(from, to, contents);
-});
-
-
-
-app.listen(5000, () => {
-    console.log('Server started on port 5000');
-});
+outbound.send_email(from, to, contents);
