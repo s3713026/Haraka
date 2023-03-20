3c6a1837e32b2a483b36f01f364035bb4de65992
AkadigitalProject <phucuong200297@gmail.com>
2023-03-20 17:33:59 +0700

    2023.local.27

--- test.js
+++ test.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const plugin = require('./plugins')
 
-app.get('/', (req, res) => {
-    res.send('SEND MAIL ĐI');
-    console.log("Hello ")
-    plugin.registered_hooks();
-});
+// app.get('/', (req, res) => {
+//     res.send('SEND MAIL ĐI');
+//     console.log("Hello ")
+// });
 
 
 
-app.listen(5000, () => {
-    console.log('Server started on port 5000');
-});
+// app.listen(5000, () => {
+//     console.log('Server started on port 5000');
+// });
+
+plugins.run_hooks('connect_init');
