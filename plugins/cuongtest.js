9adc6841f3f92cabcec9e6e802fba3b4fba30d65
AkadigitalProject <phucuong200297@gmail.com>
2023-04-09 10:32:43 +0700

    local

--- plugins/cuongtest.js
+++ plugins/cuongtest.js
@@ -1,100 +1,5 @@
-// const http = require('http');
-// const url = require('url');
-// const querystring = require('querystring');
 const outbound = require('./outbound');
-
-// //Xác định plugin: 
-// //Xác định plugin bằng cách sử dụng hàm export.register. 
-// //Hàm này có hai tham số: tên plugin và chức năng xác định hành vi của plugin.
-// // exports.register = function() {
-// //     // this.logfile = fs.createWriteStream('swaks.log', { flags: 'a' });
-// //     const server = http.createServer(function(req, res) {
-// //         const parsedUrl = url.parse(req.url);
-// //         const parsedQuery = querystring.parse(parsedUrl.query);
-// //         // Plugin code for handling the POST request here
-// //     });
-
-// //     server.listen(5000);
-// // };
-
-// exports.hook_data = function(next, connection) {
-//     const messageStream = connection.transaction.message_stream;
-
-//     let messageContent = '';
-//     messageStream.on('data', function(chunk) {
-//         messageContent += chunk;
-//     });
-
-//     messageStream.on('end', function() {
-//         const postData = {
-//             content: messageContent
-//         };
-
-//         const options = {
-//             method: 'POST',
-//             hostname: 'localhost',
-//             path: '/send-mail',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Content-Length': JSON.stringify(postData).length
-//             }
-//         };
-
-//         const req = http.request(options, function(res) {
-//             // Handle the API response
-//             console.log(res.body);
-
-//             const from = req.body.from;
-//             const to = req.body.to;
-//             const subject = req.body.subject;
-//             const body = req.body.body;
-//             //    var from = 'sender@demo.akadigital.net';
-//             //    var to = 'phucuong200297@gmail.com';
-//             //    var subject = 'Test Email C++';
-//             //    var body = 'This is a test email message.';
-//             const message = [
-//                 "From: " + from,
-//                 "To: " + to,
-//                 "MIME-Version: 1.0",
-//                 "Content-type: text/plain; charset=us-ascii",
-//                 "Subject: " + subject,
-//                 "",
-//                 body,
-//                 ""
-//             ].join("\n");
-
-//             outbound.send_email(from, to, message, (err, result) => {
-//                 if (err) {
-//                     console.error('Error sending email:', err);
-//                     res.status(500).send('Error sending email');
-//                 } else {
-//                     console.log('Email sent successfully:', result);
-//                     res.status(200).send('Email sent successfully');
-//                 }
-//             });
-//         });
-
-//         req.on('error', function(e) {
-//             // Handle the error
-//         });
-
-//         // req.write(JSON.stringify(postData));
-//         req.end();
-
-//         next();
-//     });
-// };
-
-
-
-// //Khi Haraka nhận được email, hàm hook_data_post được gọi và chi tiết email được ghi vào tệp.
-// // exports.hook_data_post = function(next, connection) {
-// //     // send mail with defined transport object
-// //     const logline = `Date: ${new Date().toString()}, From: ${connection.transaction.mail_from}, To: ${connection.transaction.rcpt_to}`;
-// //     this.logfile.write(logline + '\n');
-// //     next();
-// // };
-
+const mailcomposer = require('mailcomposer');
 const http = require('http');
 const { stringify } = require('querystring');
 
@@ -103,9 +8,6 @@ exports.register = function() {
     const server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-
-
-
         if (req.method === 'POST' && req.url === '/api/send-email') {
             let body = '';
             req.on('data', (chunk) => {
@@ -115,17 +17,25 @@ exports.register = function() {
                 const data = JSON.parse(body);
                 const { from, to, subject, text, html } = data;
                 res.end(stringify(data));
-                const message = [
-                    "From: " + from,
-                    "To: " + to,
-                    "MIME-Version: 1.0",
-                    "Content-type: text/plain; charset=us-ascii",
-                    "Subject: " + subject,
-                    "",
-                    text,
-                    html,
-                    ""
-                ].join("\n");
+                // const message = [
+                //     "From: " + from,
+                //     "To: " + to,
+                //     "MIME-Version: 1.0",
+                //     "Content-type: text/plain; charset=us-ascii",
+                //     "Subject: " + subject,
+                //     "",
+                //     text,
+                //     html,
+                //     ""
+                // ].join("\n");
+                const message = mailcomposer({
+                    from: from,
+                    to: to,
+                    subject: subjec,
+                    text: text,
+                    html: html
+                });
+
 
                 outbound.send_email(from, to, message, (err, result) => {
                     if (err) {
