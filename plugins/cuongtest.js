50e3acf448a208bcf79116ef7fb9bcfb37edaaf6
AkadigitalProject <phucuong200297@gmail.com>
2023-04-11 15:39:35 +0700

    local

--- plugins/cuongtest.js
+++ plugins/cuongtest.js
@@ -68,15 +68,3 @@ exports.register = function() {
 exports.hook_rcpt = function(next, connection, params) {
     next();
 };
-
-exports.hook_data = function(next, connection) {
-    // Generate a new Message-Id
-    var newMessageId = '<' + 'akadigital2023@' + connection.notes.hostName + 'akadigital.net' + '>';
-    // Modify the headers
-    connection.transaction.message_id = newMessageId;
-    connection.transaction.remove_header('Message-Id');
-    connection.transaction.add_header('Message-Id', newMessageId);
-
-    // Continue processing
-    next();
-};
