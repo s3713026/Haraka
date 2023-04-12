a72408222961843a43f91db74483939ce17624a1
AkadigitalProject <phucuong200297@gmail.com>
2023-04-12 11:03:12 +0700

    local

--- plugins/sendmail.js
+++ plugins/sendmail.js
@@ -4,7 +4,7 @@ exports.register = function() {
     this.loginfo('Registering sendmail plugin');
     const transporter = nodemailer.createTransport({
         host: 'demo.akadigital.net',
-        port: 25,
+        port: 587,
         secure: false,
         tls: {
             rejectUnauthorized: false
