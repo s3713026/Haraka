a18d8fbab983e680357e4c7279786f88eee24097
AkadigitalProject <phucuong200297@gmail.com>
2023-04-12 11:01:50 +0700

    local

--- plugins/sendmail.js
+++ plugins/sendmail.js
@@ -2,11 +2,6 @@ const nodemailer = require('nodemailer');
 
 exports.register = function() {
     this.loginfo('Registering sendmail plugin');
-}
-
-exports.hook_queue = function(next, connection) {
-    const recipients = connection.transaction.rcpt_to.map(to => to.address());
-
     const transporter = nodemailer.createTransport({
         host: 'demo.akadigital.net',
         port: 25,
@@ -34,3 +29,34 @@ exports.hook_queue = function(next, connection) {
         next();
     });
 }
+
+// exports.hook_queue = function(next, connection) {
+//     const recipients = connection.transaction.rcpt_to.map(to => to.address());
+
+//     const transporter = nodemailer.createTransport({
+//         host: 'demo.akadigital.net',
+//         port: 25,
+//         secure: false,
+//         tls: {
+//             rejectUnauthorized: false
+//         }
+//     });
+
+//     const mailOptions = {
+//         from: 'aka@demo.akadigital.net',
+//         // to: recipients.join(','),
+//         to: 'phucuong200297@gmail.com',
+//         subject: 'New message',
+//         text: 'Hello world!'
+//     };
+
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             connection.logerror(`Failed to send email: ${error}`);
+//         } else {
+//             connection.loginfo(`Email sent: ${info.messageId}`);
+//         }
+
+//         next();
+//     });
+// }
