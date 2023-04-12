f1d5f8aa72ef60d00bc45fab2347f659b1a83c20
AkadigitalProject <phucuong200297@gmail.com>
2023-04-12 10:31:01 +0700

    local

--- plugins/sendmail.js
+++ plugins/sendmail.js
@@ -1,82 +1,36 @@
 const nodemailer = require('nodemailer');
 
-// const from = 'sender@demo.akadigital.net';
-// const to = 'phucuong200297@gmail.com';
-// const subject = 'Test Email';
-// const body = 'This is a test email message.';
-// const transporter = nodemailer.createTransport("SMTP", {
-//     host: 'demo.akadigital.net',
-//     port: 25,
-//     secure: false,
-//     auth: {
-//         user: 'cuong@demo.akadigital.com',
-//         pass: 'password2'
-//     }
-// });
-// var from = 'sender@demo.akadigital.net';
-// var to = 'phucuong200297@gmail.com';
-// var subject = 'Test Email C';
-// var body = 'This is a test email message.';
+exports.register = function() {
+    this.loginfo('Registering sendmail plugin');
+}
 
+exports.hook_queue = function(next, connection) {
+    const recipients = connection.transaction.rcpt_to.map(to => to.address());
 
-// const mailOptions = {
-//     from: from,
-//     to: to,
-//     subject: subject,
-//     text: body,
-//     html: '<p>HTML message</p>'
-// };
+    const transporter = nodemailer.createTransport({
+        host: 'your-smtp-hostname',
+        port: 587,
+        secure: false,
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
 
+    const mailOptions = {
+        from: 'aka@demo.akadigital.net',
+        // to: recipients.join(','),
+        to: 'phucuong200297@gmail.com',
+        subject: 'New message',
+        text: 'Hello world!'
+    };
 
-// transporter.sendMail(mailOptions, function(error, info) {
-//     if (error) {
-//         console.log(error);
-//     } else {
-//         console.log('Email sent: ' + info.response);
-//     }
-// });
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            connection.logerror(`Failed to send email: ${error}`);
+        } else {
+            connection.loginfo(`Email sent: ${info.messageId}`);
+        }
 
-
-
-
-// Create a SMTP transport object
-const transporter = nodemailer.createTransport({
-    host: 'localhost',
-    port: 587,
-    secure: false,
-    tls: {
-        rejectUnauthorized: false
-    }
-});
-
-console.log('SMTP Configured');
-
-// Message object
-var message = {
-
-    // sender info
-    from: 'aka@demo.akadigital.net',
-
-    // Comma separated list of recipients
-    to: 'phucuong200297@gmail.com',
-
-    // Subject of the message
-    subject: 'Nodemailer is unicode friendly ✔',
-
-    // plaintext body
-    text: 'Hello to myself!',
-
-    // HTML body
-    html: '<p><b>Hello</b> to myself <img src="cid:note@node"/></p>' +
-        '<p>Here\'s a nyan cat for you as an embedded attachment:<br/></p>'
-};
-
-console.log('Sending Mail');
-transporter.sendMail(message, function(error) {
-    if (error) {
-        console.log('Error occured');
-        console.log(error.message);
-        return;
-    }
-    console.log('Message sent successfully!');
-});
+        next();
+    });
+}
