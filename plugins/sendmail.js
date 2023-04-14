b487e09666aba1eb25e9b1a0558eef0ff12c5ac3
AkadigitalProject <phucuong200297@gmail.com>
2023-04-14 14:15:22 +0700

    local

--- plugins/sendmail.js
+++ plugins/sendmail.js
@@ -1,5 +1,66 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
+// // sử dụng để chạy API bằng http 
+// const http = require('http');
+// const { stringify } = require('querystring');
+
+
+// exports.register = function() {
+//     //Tạo server cho API gửi email
+//     const server = http.createServer((req, res) => {
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type', 'text/plain');
+//         console.log('hello')
+//             // Khi Api post và có source là /api/send-email thì gửi mail
+//             // server.on('listen', function() {
+//             // Create a Nodemailer transport
+//         req.on('data', (chunk) => {
+//             body += chunk.toString();
+//         });
+//         // req.on('end', async() => {
+//         //     const transporter = nodemailer.createTransport({
+//         //         host: 'demo.akadigital.net',
+//         //         port: 25,
+//         //         secure: false,
+//         //         // auth: {
+//         //         //     user: 'username1',
+//         //         //     pass: 'akatestpassword'
+//         //         // }
+//         //     });
+
+//         //     // Send an email
+//         //     transporter.sendMail({
+//         //         from: 'sender@demo.akadigital.net',
+//         //         to: 'phucuong200297@gmail.com',
+//         //         subject: 'Server is running',
+//         //         text: 'The Haraka server has started.'
+//         //     });
+//         // });
+//     });
+//     // Nếu server chạy port 5000 thì đổi port API khác 
+//     server.on('error', (err) => {
+//         if (err.code === 'EADDRINUSE') {
+//             console.log('Port 5000 is already in use, trying a different port...');
+//             server.listen(0);
+//         } else {
+//             console.error(err);
+//         }
+//     });
+//     // Đổi port server cho API
+//     server.on('listening', () => {
+//         const address = server.address();
+//         console.log(`HTTP server listening on port ${address.port}`);
+//     });
+
+//     server.listen(5000);
+// };
+
+// exports.hook_rcpt = function(next, connection, params) {
+//     next();
+// };
+
+
+const outbound = require('./outbound');
 // sử dụng để chạy API bằng http 
 const http = require('http');
 const { stringify } = require('querystring');
@@ -10,32 +71,43 @@ exports.register = function() {
     const server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        console.log('hello')
-            // Khi Api post và có source là /api/send-email thì gửi mail
-            // server.on('listen', function() {
-            // Create a Nodemailer transport
-        req.on('data', (chunk) => {
-            body += chunk.toString();
-        });
-        // req.on('end', async() => {
-        //     const transporter = nodemailer.createTransport({
-        //         host: 'demo.akadigital.net',
-        //         port: 25,
-        //         secure: false,
-        //         // auth: {
-        //         //     user: 'username1',
-        //         //     pass: 'akatestpassword'
-        //         // }
-        //     });
+        // Khi Api post và có source là /api/send-email thì gửi mail
+        if (req.method === 'POST' && req.url === '/api/send-email') {
+            let body = '';
+            req.on('data', (chunk) => {
+                body += chunk.toString();
+            });
+            req.on('end', async() => {
+                const data = JSON.parse(body);
+                const { from, to, subject, text, html } = data;
+                res.end(stringify(data));
+                // Messeage gửi mail với thông tin từ API
+                const message = [
+                    "From: " + from,
+                    "To: " + to,
+                    "MIME-Version: 1.0",
+                    "Content-type: text/html; charset=utf-8",
+                    "Content-Transfer-Encoding: quoted-printable",
+                    "Subject: " + subject,
+                    "",
+                    text,
+                    html,
+                    ""
+                ].join("\n");
 
-        //     // Send an email
-        //     transporter.sendMail({
-        //         from: 'sender@demo.akadigital.net',
-        //         to: 'phucuong200297@gmail.com',
-        //         subject: 'Server is running',
-        //         text: 'The Haraka server has started.'
-        //     });
-        // });
+                // Gửi mail bằng outboud.send_email
+                outbound.send_email(from, to, message, (err, result) => {
+                    if (err) {
+                        console.error('Error sending email:', err);
+                        res.end('Error sending email');
+                    } else {
+                        console.log('Email sent successfully:', result);
+                        res.end('Email sent successfully');
+                    }
+                });
+
+            })
+        }
     });
     // Nếu server chạy port 5000 thì đổi port API khác 
     server.on('error', (err) => {
