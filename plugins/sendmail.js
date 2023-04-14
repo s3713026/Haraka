3adb3504d6a99f98ba38652204caf84e1b28e7b5
AkadigitalProject <phucuong200297@gmail.com>
2023-04-14 10:20:37 +0700

    local

--- plugins/sendmail.js
+++ plugins/sendmail.js
@@ -1,57 +1,32 @@
-// const nodemailer = require('nodemailer');
-
-// exports.register = function() {
-//     this.logdebug("registering the sendMail plugin");
-
-//     this.register_hook('server_start', 'sendMail');
-// }
-
-// exports.sendMail = function(next, connection) {
-//     const body = connection.transaction.body;
-
-//     let transporter = nodemailer.createTransport({
-//         host: 'localhost',
-//         port: 587,
-//         secure: false, // true for 465, false for other ports
-//         auth: {
-//             user: 'cuong.truong@akadigital.vn', // your email address
-//             pass: 'password3' // your email password
-//         }
-//     });
-//     su
-
-//     let mailOptions = {
-//         from: 'aka@demo.akadigital.net', // sender address
-//         to: body.header.to, // recipient address
-//         subject: body.header.subject, // Subject line
-//         text: body.bodytext, // plain text body
-//         html: body.bodytext // html body
-//     };
-
-//     transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//             this.logerror(error);
-//             return next(DENY, "Error sending email");
-//         }
-//         this.loginfo('Message sent: %s', info.messageId);
-//         return next(OK);
-//     });
-// };
+const nodemailer = require('nodemailer');
 
 exports.register = function() {
     // Register a hook that will be triggered when Haraka starts up
     this.register_hook('server_start', 'sendmail');
 }
 
-exports.sendmail = function(next, connection) {
-    // Use the 'sendmail' method of the 'sendmailer' plugin to send an email
+exports.sendmail = function(next) {
+    // Create a nodemailer transport object
+    const transporter = nodemailer.createTransport({
+        host: 'demo.akadigital.net',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'youremail@gmail.com',
+            pass: 'yourpassword'
+        }
+    });
+
+    // Create an email message
     const mailOptions = {
         from: 'sender@demo.akadigital.net',
         to: 'phucuong200297@gmail.com',
         subject: 'Haraka server started',
         text: 'The Haraka server has started up successfully.'
     };
-    connection.sendmailer.sendmail(mailOptions, function(err, info) {
+
+    // Send the email message
+    transporter.sendMail(mailOptions, function(err, info) {
         if (err) {
             console.log('Error sending email:', err);
         } else {
