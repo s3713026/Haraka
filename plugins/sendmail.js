004de58024d6d369cd86c1a9b77fcfeea87b5b8c
AkadigitalProject <phucuong200297@gmail.com>
2023-04-15 23:32:44 +0700

    lcal

--- plugins/sendmail.js
+++ plugins/sendmail.js
@@ -25,7 +25,7 @@ exports.register = function() {
                 res.end(stringify(data));
                 // Messeage gửi mail với thông tin từ API
                 const transporter = nodemailer.createTransport({
-                    host: '158.101.137.14',
+                    host: 'demo.akadigital.net',
                     port: 25,
                     secure: false,
                     auth: {
